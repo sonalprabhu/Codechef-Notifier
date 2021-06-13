ff4deb9f2da7b9aeccfc5ef0358c4549777f742c
Sonal Prabhu <sonalsprabhu16@gmail.com>
2021-06-13 12:01:19 +0530

    Poll the codechef api to get the result

--- notify.js
+++ notify.js
@@ -1,13 +1,14 @@
+
+/* check the URL from details object
+* if the url matches the required codechef url then
+* extract the submission storageKeys from the url
+* check if a request with that submission storageKeys is already present in your storage
+* The above check is necessary because codechef repeatedly sends this
+* request until the result is obtained.
+* If the submission storageKeys is not present then save request and call the function made
+* for fetching question info, fetching the result and notifying the user.
+*/
 chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-  /* check the URL from details object
-  * if the url matches the required codechef url then
-  * extract the submission storageKeys from the url
-  * check if a request with that submission storageKeys is already present in your storage
-  * The above check is necessary because codechef repeatedly sends this
-  * request until the result is obtained.
-  * If the submission storageKeys is not present then save request and call the function made
-  * for fetching question info, fetching the result and notifying the user.
-  */
   var tokenValue;
   if (details.url.indexOf('solution_id') != -1) {
     chrome.storage.local.get(['solution_id'], (storageKeys) => {
@@ -22,7 +23,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
         }
         console.log(submissionId);
         chrome.storage.local.set({ 'solution_id': submissionId });
-        getProblemDetails();
+        setTimeout(function () { checkResult({ submissionId, csrf_token: tokenValue }) }, 3000);
       }
     })
   }
@@ -32,13 +33,6 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
   ["blocking", "requestHeaders"]);
 
 
-function getProblemDetails() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { sendProblemDetails: true }, function (response) {
-      console.log(response); //Problem ID and name returned back from content script
-    });
-  });
-}
 
 //Clear localStorage before extension closes
 function clearLocalStorage() {
@@ -48,5 +42,31 @@ function clearLocalStorage() {
       console.error(error);
     }
   })
+}
+
+function checkResult({ submissionId, csrf_token }) {
+
+  $.ajax({
+
+    url: `https://www.codechef.com/api/ide/submit?solution_id=${submissionId}`,
+
+    dataType: "json",
+
+    headers: { "x-csrf-token": csrf_token },
+
+    success:
+      function getProblemDetails(data) {
+        if (data && data.result_code && data.result_code === 'accepted') {
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { sendProblemDetails: true }, function (response) {
+              const { problemName, problemCode } = response;
+              chrome.notifications.create({ title: "Results announced", message: `${problemName} : ${problemCode}`, iconUrl: "https://icon-library.net/images/retry-icon/retry-icon-20.jpg", type: "list" });
+            });
+          });
+        }
+      },
+
+    error: function () { console.log('Error occured') }/* function to handle errors*/
+  });
 }
 
